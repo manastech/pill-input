25e45a9cc64b30a9ede6a92a8b9adac7bfcc5b20
jkicillof <jkicillof@manas.com.ar>
2014-09-05 18:35:48 -0300

    new features added

--- textInput.js
+++ textInput.js
@@ -1,4 +1,4 @@
-var source, contextMenu;
+var source, contextMenu, selectedRange;
 
 function dragStartHandler(e) {
   source = e.currentTarget.parentNode;
@@ -22,74 +22,228 @@ function dragLeaveHandler(e) {
 }
 
 function dragOverHandler(e) {
-  e.preventDefault();
+  if(e.currentTarget.contenteditable != "true") {
+    e.preventDefault();
+  }
 }
 
 function dropHandler(e) {
   var data = e.dataTransfer.getData("text");
-  var target = document.getElementById(data);
-  if(target.contains(e.target)) {
+  var target = document.getElementById(data) || e.dataTransfer.getData("text/html");
+  if(target.contains && target.contains(e.target)) {
     e.preventDefault();
     return;
   }
   e.currentTarget.classList.remove("dragover");
-  target.classList.remove("block");
   switch(e.currentTarget.className) {
     case "dropZone":
-      var clone = target.cloneNode(true);
-      clone.id = guid();
-      clone.classList.add("block");
-      e.currentTarget.appendChild(clone);
+      if(e.currentTarget != source && target.nodeType == 1) {
+        var clone = target.cloneNode(true);
+        clone.id = guid();
+        e.currentTarget.appendChild(clone);
+      }
       e.preventDefault();
       break;
     default:
       if(e.target.classList.contains("input")) {
         if(e.currentTarget == source) {
-          insertAt(target, e.clientX, e.clientY);
+          insertAtPosition(target, e.clientX, e.clientY);
           e.preventDefault();
         } else {
-          var clone = target.cloneNode(true);
-          clone.id = guid();
-          insertAt(clone, e.clientX, e.clientY);
+          if(target.nodeType) {
+            var clone = target.cloneNode(true);
+            if(clone.id) {
+              clone.id = guid();
+            }
+            insertAtPosition(clone, e.clientX, e.clientY);
+          } else {
+            var wrapper = document.createElement("div");
+            wrapper.innerHTML = target;
+            var elements = [];
+            while(wrapper.firstChild) {
+              elements.push(wrapper.removeChild(wrapper.firstChild));
+            }
+            insertAtPosition(elements, e.clientX, e.clientY);
+          }
         }
       }
       break;
   }
   e.stopPropagation();
-  e.currentTarget.normalize();
+  sanitize(e.currentTarget);
 }
 
-function insertAt(element, x, y) {
-   var range, textNode, offset;
+function insertAtPosition(elements, x, y) {
+  if(!(elements instanceof Array)) {
+    elements = [elements];
+  }
+  var range, node, offset;
   if (document.caretPositionFromPoint) {
     range = document.caretPositionFromPoint(x, y);
-    textNode = range.offsetNode;
+    node = range.offsetNode;
     offset = range.offset;
   } else if (document.caretRangeFromPoint) {
     range = document.caretRangeFromPoint(x, y);
-    textNode = range.startContainer;
+    node = range.startContainer;
     offset = range.startOffset;
   }
-  if (textNode.nodeType == 3) {
-    textNode.parentNode.insertBefore(element, textNode.splitText(offset));
+  if (node.nodeType == 3) {
+    var container = node.parentNode;
+    if(container.classList.contains("input")) {
+      node = node.splitText(offset);
+      elements.forEach(function (element) {
+        if(element.nodeType) {
+          container.insertBefore(element, node);
+        }
+      });
+    } else {
+      while(container && !container.classList.contains("input")) {
+        node = container;
+        container = container.parentNode;
+      }
+      if(container) {
+        elements.forEach(function (element) {
+          container.insertBefore(element, node);
+        });
+      }
+    }
+  } else {
+     elements.forEach(function (element) {
+      node.appendChild(element);
+    });
   }
 }
 
 function keyDownHandler(e) {
   switch(e.keyCode) {
     case 8:
+      var input = e.currentTarget;
       var range = window.getSelection().getRangeAt(0);
-      var target = range.startContainer;
-      if(!range.startOffset && target.className == "pill") {
-        target.parentNode.removeChild(target);
-        e.preventDefault();
-      } else if(!range.startOffset && target.previousSibling && target.previousSibling.className == "pill") {
-        target.parentNode.removeChild(target.previousSibling);
-        e.preventDefault();
+      var target;
+      switch(range.startContainer.nodeType) {
+        case 1:
+          target = input.childNodes[range.startOffset - 1];
+          if(target) {
+            switch(target.nodeName) {
+              case "BR":
+              case "A":
+                break;
+              default:
+                target = undefined;
+                break;
+            }
+          }
+          sanitize(e.currentTarget);
+          break;
+        case 3:
+          if(!range.startOffset) {
+            target = range.startContainer.previousSibling;
+          }
+          break;
       }
-      break;
   }
-  e.currentTarget.normalize();
+  if(target) {
+    input.removeChild(target);
+    e.preventDefault();
+    sanitize(input);
+  }
+}
+
+function sanitize(element) {
+  element.normalize();
+  if(element.hasChildNodes() && element.firstChild.nodeName == "BR") {
+    element.removeChild(element.firstChild);
+  }
+}
+
+function keyUpHandler(e) {
+  displayPillButton(e.target);
+  sanitize(e.currentTarget);
+}
+
+function mouseUpHandler(e) {
+  displayPillButton(e.target);
+}
+
+function blurHandler(e) {
+  displayPillButton(e.target);
+}
+
+function displayPillButton(dispatcher) {
+  var isInput = false;
+  var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
+  var selection = false;
+  if(range && !range.collapsed && fromInput(range.startContainer) && fromInput(range.endContainer)) {
+    selection = range.startContainer == range.endContainer && range.startOffset == range.endOffset? false : true;
+  }
+  var container = dispatcher;
+  do {
+    container = container.parentNode;
+  } while(container && !container.classList.contains("example"));
+  if(container) {
+    var pillButton = container.querySelector("#pillButton");
+    pillButton.style.display = selection? "block" : "none";
+  }
+  selectedRange = selection? range : undefined;
+}
+
+function pillButtonClickHandler(e) {
+  if(selectedRange) {
+    var input = selectedRange.startContainer;
+    while(input && (input.nodeType == 3 || !input.classList.contains("input"))) {
+      input = input.parentNode;
+    }
+    var startContainer = selectedRange.startContainer;
+    if(selectedRange.startOffset) {
+      startContainer = startContainer.splitText(selectedRange.startOffset);
+    }
+    var endContainer = selectedRange.endContainer;
+    if(endContainer.nodeType == 3 && selectedRange.endOffset < endContainer.data.length) {
+      endContainer.splitText(selectedRange.endOffset);
+    }
+    var insertTarget = endContainer.nextSibling;
+    var label = "";
+    var elements = [];
+    for (var element = startContainer; element != endContainer.nextSibling; element = element.nextSibling) {
+      switch(element.nodeType) {
+        case 1:
+          if(element.classList.contains("pill")) {
+            label += element.getAttribute("data-label");
+          } else {
+            label += element.textContent;
+          }
+          break;
+        case 3:
+          label += element.data;        
+          break;
+      }
+      elements.push(element);
+    }
+    elements.forEach(function (element) {
+       if(element == input) {
+        while(input.hasChildNodes()) {
+          input.removeChild(input.lastChild);
+        }
+      } else {
+        input.removeChild(element);
+      }
+    });
+    var pill = createPill(label);
+    if(insertTarget && input.contains(insertTarget)) {
+      input.insertBefore(pill, insertTarget);
+    } else {
+      input.appendChild(pill);
+    }
+  }
+}
+
+function fromInput(element) {
+  var result = false;
+  do {
+    result = element.classList && element.classList.contains("input");
+    element = element.parentNode;
+  } while(!result && element);
+  return result;
 }
 
 function contextMenuHandler(e) {
@@ -110,7 +264,10 @@ function contextMenuHandler(e) {
   addOption(contextMenu, "Average", "average", e.currentTarget);
   addOption(contextMenu, "None", undefined, e.currentTarget);
   addOption(contextMenu, "Break", undefined, e.currentTarget, function() {
-    var pill = e.target.parentNode;
+    var pill = e.target;
+    while(pill && !pill.classList.contains("pill")) {
+      pill = pill.parentNode;
+    }
     var container = pill.parentNode;
     var textNode = document.createTextNode(pill.getAttribute("data-label"));
     container.replaceChild(textNode, pill);
