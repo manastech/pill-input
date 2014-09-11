961bc005ed43f27e4e9efb26729c0498bd6b5cfd
jkicillof <jkicillof@manas.com.ar>
2014-09-11 13:20:43 -0300

    stable version

--- textInput.js
+++ textInput.js
@@ -36,11 +36,10 @@ var BLUR = "blur";
 var COPY = "copy";
 var CUT = "cut";
 
-var source, relatedElement, menu;
+var dragSource, dragElement, menu;
 
 function Pill(label) {
   var pill = document.createElement(A);
-  pill.id = guid();
   pill.draggable = true;
   pill.className = PILL;
   pill.contentEditable = "false";
@@ -114,46 +113,16 @@ function hideMenu() {
   }
 }
 
-function displayPillButton(dispatcher) {
-  if(!getAncestor(INPUT, dispatcher)) return;
-  var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
-  var container = getAncestor(EXAMPLE, dispatcher);
-  if (container) {
-    var pillButton = container.querySelector("." + PILL_BUTTON);
-    pillButton.style.display = range && !range.collapsed? "block" : "none";
-    var input = container.querySelector("." + INPUT);
-    for (var element = input.firstChild; element != null; element = element.nextSibling) {
-      if (element.classList && element.classList.contains(PILL)) {
-        element.classList.remove(FOCUS);
-      }
-    }
-  }
-  if (range && !range.collapsed) {
-    for (var element = range.startContainer; element && element != range.endContainer.nextSibling; element = element.nextSibling) {
-      if (element && element.classList && element.classList.contains(PILL)) {
-        element.classList.add(FOCUS);
-      }
-    }
-  }
-}
-
 function insertAtPosition(elements, x, y) {
   if (!(elements instanceof Array)) {
     elements = [elements];
   }
-  var range, node, offset;
-  if (document.caretPositionFromPoint) {
-    range = document.caretPositionFromPoint(x, y);
-    node = range.offsetNode;
-    offset = range.offset;
-  } else if (document.caretRangeFromPoint) {
-    range = document.caretRangeFromPoint(x, y);
-    node = range.startContainer;
-    offset = range.startOffset;
-  }
+  var range = getRangeFromPoint(x, y);
+  var node = range.offsetNode || range.startContainer;
+  var offset = range.offset || range.startOffset;
   if (node.nodeType == Node.TEXT_NODE) {
-    node = node.splitText(range.startOffset);
     var container = node.parentNode;
+    node = node.splitText(offset);
     elements.forEach(function (element) {
       container.insertBefore(element, node);
     });
@@ -168,11 +137,10 @@ function insert(elements) {
   if (!(elements instanceof Array)) {
     elements = [elements];
   }
-  var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
+  var range = getRange();
   if(range && !range.collapsed) {
-    trim(range);
     deleteSelection();
-    var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
+    var range = getRange();
   }
   var node = range.startContainer;
   if (node.nodeType == Node.TEXT_NODE) {
@@ -195,10 +163,10 @@ function insert(elements) {
 }
 
 function createPill() {
-  var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
-  if (range && !range.collapsed && getAncestor(INPUT, range.startContainer) && getAncestor(INPUT, range.endContainer)) {
-    var elements = trim(range);
+  var range = getRange();
+  if (inputContainsRange(range)) {
     var insertTarget = range.endContainer.nextSibling;
+    var elements = toArray(getElementsFromRange(range).childNodes);
     var input = getAncestor(INPUT, range.endContainer);
     var label = "";
     elements.forEach(function (element) {
@@ -206,19 +174,12 @@ function createPill() {
         case Node.ELEMENT_NODE:
           if (element.classList.contains(PILL)) {
             label += element.getAttribute(DATA_LABEL);
-            input.removeChild(element);
           } else {
             label += element.textContent;
-            if (element == input) {
-              while(input.hasChildNodes()) {
-                input.removeChild(input.lastChild);
-              }
-            }
           }
           break;
         case Node.TEXT_NODE:
           label += element.data;        
-          input.removeChild(element);
           break;
       }
     });
@@ -238,98 +199,54 @@ function sanitize(element) {
   }
 }
 
-function trim(range) {
-  if (!range) return;
-  var startContainer = range.startContainer;
-  if (range.startOffset) {
-    startContainer = startContainer.splitText(range.startOffset);
-    range.setStart(startContainer, 0);
-  }
-  var endContainer = range.endContainer;
-  if (endContainer.nodeType == Node.TEXT_NODE && range.endOffset < endContainer.data.length) {
-    endContainer.splitText(range.endOffset);
-    range.setEnd(endContainer, endContainer.data.length);
-  }
-  clearSelection();
-  window.getSelection().addRange(range);
-  var elements = [];
-  for (var element = startContainer; element && element != endContainer.nextSibling; element = element.nextSibling) {
-    elements.push(element);
-  }
-  return elements;
-}
-
-function copy(range) {
-  var elements = [];
-  if (range && getAncestor(INPUT, range.startContainer) && getAncestor(INPUT, range.endContainer)) {
-    if(range.startContainer == range.endContainer) {
-      switch(range.startContainer.nodeType) {
-        case Node.TEXT_NODE:
-          var substring = range.startContainer.data.substring(range.startOffset, range.endOffset);
-          elements.push(document.createTextNode(substring));
-          break;
-        case Node.ELEMENT_NODE:
-          for (var index = range.startOffset; index < range.endOffset; index++) {
-            var node = element.childNodes[index];
-            elements.push(node.cloneNode(true));
-          }
-          break;
-      }
-    } else {
-      for (var node = range.startContainer; node && node != range.endContainer.nextSibling; node = node.nextSibling) {
-        switch(node.nodeType) {
-          case Node.TEXT_NODE:
-            var substring;
-            if(node == range.startContainer) {
-              substring = node.data.substring(range.startOffset, node.data.length);
-            } else if (node == range.endContainer) {
-              substring = node.data.substring(0, range.endOffset);
-            } else {
-              substring = node.data;
-            }
-            elements.push(document.createTextNode(substring));
-            break;
-          case Node.ELEMENT_NODE:
-            elements.push(node.cloneNode(true));
-            break;
-        }
-      }
+function inputContainsRange(range) {
+  var result = false;
+  if(range && !range.collapsed) {
+    if(getAncestor(INPUT, range.commonAncestorContainer)) {
+      result = true;
     }
-    return elements;
   }
-  if (range.startOffset) {
-    startContainer = startContainer.splitText(range.startOffset);
-    range.setStart(startContainer, 0);
-  }
-  var endContainer = range.endContainer;
-  if (endContainer.nodeType == Node.TEXT_NODE && range.endOffset < endContainer.data.length) {
-    endContainer.splitText(range.endOffset);
-    range.setEnd(endContainer, endContainer.data.length);
-  }
-  clearSelection();
-  window.getSelection().addRange(range);
-  var elements = [];
-  for (var element = startContainer; element && element != endContainer.nextSibling; element = element.nextSibling) {
-    elements.push(element);
-  }
-  return elements;
+  return result;
 }
 
-function deleteSelection() {
+function intersectsRange(node, range) {
+  var result = false;
+  if(range) {
+    result = range.intersectsNode(node);
+  }
+  return result;
+}
+
+function getRange() {
+  var range;
   if (window.getSelection) {
-    var selection = window.getSelection();
-    selection.deleteFromDocument ();
-    if (!selection.isCollapsed) {
-      selection.getRangeAt(0).deleteContents();
-    }
-    if (selection.anchorNode) {
-      selection.collapse (selection.anchorNode, selection.anchorOffset);
-    }
-  } else {
-    if (document.selection) {
-      document.selection.clear();
-    }
+    range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
   }
+  return range;
+}
+
+function getRangeFromPoint(x, y) {
+  var range;
+  if (document.caretPositionFromPoint) {
+    range = document.caretPositionFromPoint(x, y);
+  } else if (document.caretRangeFromPoint) {
+    range = document.caretRangeFromPoint(x, y);
+  }
+  return range;
+}
+
+function getElementsFromRange(range, clone) {
+  var elements;
+  if(clone) {
+    elements = range.cloneContents();
+  } else {
+    elements = range.extractContents();
+  }
+  return elements;
+}
+
+function setSelection(range) {
+  window.getSelection().addRange(range);
 }
 
 function clearSelection() {
@@ -340,23 +257,33 @@ function clearSelection() {
   }
 }
 
+function deleteSelection() {
+  if (window.getSelection) {
+    var selection = window.getSelection();
+    selection.deleteFromDocument();
+    if(!selection.isCollapsed) {
+      selection.collapseToEnd();
+    }
+  } else {
+    if (document.selection) {
+      document.selection.clear();
+    }
+  }
+}
+
+function rectangleIntersetcs(rectangle, x, y) {
+  return rectangle.left < x && x < rectangle.right && rectangle.top < y && y < rectangle.bottom;
+}
+
 function getAncestor(className, node) {
   var ancestor;
-  do {
+  while (node && node != document && !ancestor) {
     if (node.classList && node.classList.contains(className)) {
       ancestor = node;
     }
     node = node.parentNode;
-  } while (node && node != document && !ancestor);
+  };
   return ancestor;
-}
-
-function guid() {
-  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-}
-
-function S4() {
-  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 }
 
 function toArray(obj) {
@@ -365,32 +292,67 @@ function toArray(obj) {
   });
 };
 
+function documentFragmentToString(documentFragment) {
+  var string = "";
+  toArray(documentFragment.childNodes).forEach(function (element) {
+    switch(element.nodeType) {
+      case Node.ELEMENT_NODE:
+        string += element.outerHTML;
+        break;
+      case Node.TEXT_NODE:
+        string += element.data;
+        break;
+    }
+  });
+  return string;
+}
+
 //Handlers
 
-function dragStartHandler(e) {
-  var data = "";
-  var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
-  var pill = getAncestor(PILL, e.target);
-  if (range && getAncestor(INPUT, range.startContainer) && getAncestor(INPUT, range.endContainer)) {
-    var elements = copy(range);
-    elements.forEach(function (element) {
-      switch(element.nodeType) {
-        case Node.TEXT_NODE:
-          data += element.data;
-          break;
-        case Node.ELEMENT_NODE:
-          data += element.outerHTML;
-          break;
-      }
-    });
-    relatedElement = elements;
-    source = getAncestor(INPUT, range.startContainer);
-  } else if(pill) {
-    data = pill.outerHTML;
-    relatedElement = pill;
-    source = pill.parentNode;
+function selectHandler(dispatcher) {
+  if(!getAncestor(INPUT, dispatcher)) return;
+  var range = getRange();
+  var rect = range && range.getBoundingClientRect();
+  var container = getAncestor(EXAMPLE, dispatcher);
+  if (container) {
+    var pillButton = container.querySelector("." + PILL_BUTTON);
+    pillButton.style.display = range && !range.collapsed? "block" : "none";
+    var input = container.querySelector("." + INPUT);
+    
   }
-  if(data.length) {
+  toArray(input.childNodes).forEach(function (element) {
+    if(element.nodeType == Node.ELEMENT_NODE) {
+      if(range && range.intersectsNode(element) && rect.width) {
+        element.classList.add(FOCUS);
+      } else if(element.classList.contains(PILL) && range && range.collapsed && !range.startOffset && range.startContainer.previousSibling == element) {
+        element.classList.add(FOCUS);
+      } else {
+        element.classList.remove(FOCUS);
+      }
+    }
+  });
+}
+
+function dragStartHandler(e) {
+  var range = getRange();
+  var pill = getAncestor(PILL, e.target);
+  var data;
+  if(pill) {
+    if(inputContainsRange(range) && intersectsRange(pill, range)) {
+      dragElement = getElementsFromRange(range, true);
+      dragSource = getAncestor(INPUT, range.commonAncestorContainer);
+      data = documentFragmentToString(dragElement);
+    } else {
+      dragElement = pill;
+      dragSource = pill.parentNode;
+      data = pill.outerHTML;
+    }
+  } else if (inputContainsRange(range)) {
+    dragElement = getElementsFromRange(range, true);
+    dragSource = getAncestor(INPUT, range.commonAncestorContainer);
+    data = documentFragmentToString(dragElement);
+  }
+  if(data) {
     e.dataTransfer.effectAllowed = MOVE;
     e.dataTransfer.setData(TEXT, data);
   }
@@ -398,6 +360,7 @@ function dragStartHandler(e) {
 
 function dragEndHandler(e) {
   document.activeElement.blur();
+  dragSource = undefined;
 }
 
 function dragEnterHandler(e) {
@@ -418,7 +381,7 @@ function dragLeaveHandler(e) {
 
 function dragOverHandler(e) {
   var target = e.target;
-  if (target.contenteditable != "true") {
+  if (target.contenteditable != "true" && !bowser.firefox) {
     e.preventDefault();
   }
 }
@@ -429,36 +392,26 @@ function dropHandler(e) {
   wrapper.innerHTML = e.dataTransfer.getData(TEXT);
   target.classList.remove(DRAG_OVER);
   if (target.classList.contains(DROP_ZONE)) {
-    if (target != source) {
+    if (target != dragSource) {
       var pills = toArray(wrapper.querySelectorAll("." + PILL));
       pills.forEach(function (pill) {
         target.appendChild(pill);
       });
     }
-    e.preventDefault();
   } else if (target.classList.contains(INPUT)) {
     var elements = toArray(wrapper.childNodes);
-    if (target != source) {
-      elements.forEach(function (element) {
-        if (element.id) {
-          element.id = guid();
-        }  
-      });
-    } else {
-      if(relatedElement.classList && relatedElement.classList.contains(PILL)) {
-        relatedElement.parentNode.removeChild(relatedElement);
-      }else {
-        deleteSelection();
-      };
-    }
     insertAtPosition(elements, e.clientX, e.clientY);
-    e.preventDefault();
+    if (target == dragSource) {
+      deleteSelection();
+      if(dragElement.classList && dragElement.classList.contains(PILL)) {
+        dragElement.parentNode.removeChild(dragElement);
+      }
+    }
   }
   sanitize(target);
   clearSelection();
   e.stopPropagation();
-  relatedElement = undefined;
-  source = undefined;
+  e.preventDefault();
 }
 
 function keyDownHandler(e) {
@@ -466,7 +419,7 @@ function keyDownHandler(e) {
   if(!input) return;
   switch(e.keyCode) {
     case 8:
-      var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
+      var range = getRange();
       if(range && range.collapsed) {
         var target;
         switch(range.startContainer.nodeType) {
@@ -496,13 +449,13 @@ function keyDownHandler(e) {
     input.removeChild(target);
     e.preventDefault();
   }
-  displayPillButton(input);
+  window.setTimeout(selectHandler, 25, input);
 }
 
 function keyUpHandler(e) {
   var input = getAncestor(INPUT, e.target);
   if(!input) return;
-  displayPillButton(input);
+  selectHandler(input);
   sanitize(input);
 }
 
@@ -510,13 +463,13 @@ function clickHandler(e) {
   if(e.target.classList.contains(PILL_BUTTON)) {
     createPill();
     clearSelection();
-    displayPillButton(getAncestor(INPUT, e.target.parentNode.querySelector("." + INPUT)));
+    selectHandler(getAncestor(INPUT, e.target.parentNode.querySelector("." + INPUT)));
   } else {
     var pill = getAncestor(PILL, e.target);
     var input = getAncestor(INPUT, e.target);
     var arrow = e.target.className == ARROW;
     var rightButton = e.which == 3;
-    window.setTimeout(displayPillButton, 50, input);
+    selectHandler(input);
     if(!pill || (!arrow && !rightButton)) return;
     if (e.type == CONTEXT_MENU) e.preventDefault();
     hideMenu();
@@ -531,29 +484,29 @@ function mouseDownHandler(e) {
   }
   document.addEventListener(MOUSE_MOVE, mouseMoveHandler);
   document.addEventListener(MOUSE_UP, mouseUpHandler);
-  displayPillButton(e.target);
+  selectHandler(e.target);
 }
 
 function mouseMoveHandler(e) {
-  displayPillButton(e.target);
+  selectHandler(e.target);
 
 }
 
 function mouseUpHandler(e) {
   document.removeEventListener(MOUSE_MOVE, mouseMoveHandler);
   document.removeEventListener(MOUSE_UP, mouseUpHandler);
-  displayPillButton(e.target);
+  selectHandler(e.target);
 }
 
 function blurHandler(e) {
-  displayPillButton(e.target);
+  selectHandler(e.target);
 }
 
 function copyHandler(e) {
   var input = getAncestor(INPUT, e.target);
-  var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
-  if(input && range && !range.collapsed && getAncestor(INPUT, range.startContainer) && getAncestor(INPUT, range.endContainer)) {
-    var elements = copy(range);
+  var range = getRange();
+  if(inputContainsRange(range)) {
+    var elements = toArray(getElementsFromRange(range, true).childNodes);
     var data = "";
     elements.forEach(function (element) {
       switch(element.nodeType) {
@@ -573,7 +526,7 @@ function copyHandler(e) {
 function cutHandler(e) {
   copyHandler(e);
   var input = getAncestor(INPUT, e.target);
-  var range = window.getSelection().rangeCount? window.getSelection().getRangeAt(0) : undefined;
+  var range = getRange();
   if(input && range) {
     deleteSelection();
   }
@@ -582,17 +535,15 @@ function cutHandler(e) {
 function pasteHandler(e) {
   var input = getAncestor(INPUT, e.target);
   if(input) {
-    var elements = [];
     var wrapper = document.createElement(DIV);
     wrapper.innerHTML = e.clipboardData.getData(TEXT);
-    while(wrapper.hasChildNodes()) {
-      elements.push(wrapper.removeChild(wrapper.firstChild));
-    }
+    var elements =  toArray(wrapper.childNodes);
     insert(elements);
     var range = document.createRange();
     range.setStartAfter(elements[elements.length - 1]);
     clearSelection();
-    window.getSelection().addRange(range);
+    setSelection(range);
+    selectHandler(input)
     e.preventDefault()
   }
 }
